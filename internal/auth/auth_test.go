d2873da88e6c52c96a63a37b6e023e8e2ea9fd20
copartner6412 <foothold4321@tutamail.com>
2025-01-28 22:14:29 +0330

    Added unit test for internal/auth.GetAPIKey

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,48 @@
+package auth_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+)
+
+func TestGetAPIKeyFailsForInvalidInput(t *testing.T) {
+	testCases := map[string]http.Header{
+		"nil_header":  nil,
+		"without_key": {},
+		"without_authorization_key": {
+			"another": []string{"another"},
+		},
+		"nil_value": {
+			"Authorization": nil,
+		},
+		"no_value": {
+			"Authorization": []string{},
+		},
+		"value_one_empty": {
+			"Authorization": []string{
+				"",
+			},
+		},
+		"value_one_part": {
+			"Authorization": []string{
+				"ApiKey",
+			},
+		},
+		"value_wrong_first_part": {
+			"Authorization": []string{
+				"apiKey token",
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			if _, err := auth.GetAPIKey(testCase); err != nil {
+				t.Error(err)
+			}
+		})
+
+	}
+}
